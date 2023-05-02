9283a267430911eaff3c1aed5fada51adfba667e
Kocsis Tamás <ktomi87@gmail.com>
2023-05-02 17:31:19 +0200

    login update

--- app/src/main/java/hu/larzo/asvany/CartNotificationService.java
+++ app/src/main/java/hu/larzo/asvany/CartNotificationService.java
@@ -3,20 +3,26 @@ package hu.larzo.asvany;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.IBinder;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
-public class CartNotificationService {
+public class CartNotificationService extends Service {
     private static final String CHANNEL_ID = "shop_notification_channel";
     private final int NOTIFICATION_ID = 0;
 
     private NotificationManager mNotifyManager;
     private Context mContext;
 
+    public CartNotificationService() {
+        super();
+    }
 
     public CartNotificationService(Context context) {
         this.mContext = context;
@@ -53,5 +59,11 @@ public class CartNotificationService {
 
     public void cancel() {
        mNotifyManager.cancel(NOTIFICATION_ID);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
--- app/src/main/java/hu/larzo/asvany/LoginActivity.java
+++ app/src/main/java/hu/larzo/asvany/LoginActivity.java
@@ -1,18 +1,30 @@
 package hu.larzo.asvany;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends BaseActivity {
 
+    private TextView Email;
+    private TextView Password;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         // Keressük meg a "Új ügyfél vagyok" gombot
         Button registerButton = findViewById(R.id.register_button);
@@ -30,6 +42,39 @@ public class LoginActivity extends BaseActivity {
             }
         });
 
+        Button loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+
+    private void login(){
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.password);
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+
+                    // Indítsuk el a RegisterActivity-t
+                    startActivity(mainIntent);
+                    overridePendingTransition(0, 0);
+                }else {
+                    Toast.makeText(
+                            LoginActivity.this,
+                            "Sikertelen bejelentkezés: " + task.getException().getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
